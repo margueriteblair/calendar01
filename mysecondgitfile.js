0088e4e46d891d7a67b4b93a1581496e4bb24072
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-05 21:28:59 -0400

    first operations added

---
+++ mysecondgitfile.js
@@ -0,0 +1,8 @@
+function calc(x, y, operation) {
+    if (operation === "add") {
+        console.log(x+y);
+    } else if (operation === "multiply") {
+        console.log(x*y);
+} };
+
+calc(5, 7, "multiply");
