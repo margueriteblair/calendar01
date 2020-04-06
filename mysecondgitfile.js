bfe0ccdb4b5333f88ca8986603d693f4eec24cd4
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-05 22:15:49 -0400

    on lines 6 & 8 added division and subtraction

--- mysecondgitfile.js
+++ mysecondgitfile.js
@@ -3,6 +3,11 @@ function calc(x, y, operation) {
         console.log(x+y);
     } else if (operation === "multiply") {
         console.log(x*y);
-} };
+    } else if (operation === "divide") {
+        console.log(x/y);
+    } else if (operation === "subtract") {
+        console.log(x-y);
+    }
+ };
 
-calc(5, 7, "multiply");
+calc(5, 7, "subtract");
